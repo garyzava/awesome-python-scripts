0daf5a84d58147524ac8d04131a6a3534fcabc5f
garyzava <garyzava@users.noreply.github.com>
2022-01-06 13:23:21 +0400

    Create parallelism.py

---
+++ parallelism.py
@@ -0,0 +1,48 @@
+# Source: https://stackoverflow.com/questions/7207309/how-to-run-functions-in-parallel
+# Workflow Pipeline: 
+# func0 -> func1 -> func3
+#	      -> func2	
+
+from multiprocessing import Process
+
+def func0():
+  print("func0: starting")
+  for i in range(10000000): pass
+  print("func0: finishing")
+
+def func1():
+  print("func1: starting")
+  for i in range(10000000): pass
+  print("func1: finishing")
+
+def func2():
+  print("func2: starting")
+  for i in range(1000000000): pass
+  print("func2: finishing")
+
+def func3():
+  print("func3: starting")
+  for i in range(10000000): pass
+  print("func3: finishing")
+
+def runInParallel(*fns):
+  proc = []
+  for fn in fns:
+    p = Process(target=fn)
+    p.start()
+    proc.append(p)
+  for p in proc:
+    p.join()
+
+if __name__ == '__main__':
+
+  p0 = Process(target=func0)
+  p3 = Process(target=func3)
+  #func0 starts & ends
+  p0.start()  
+  p0.join()
+  #func1 & func2 start and end in parallel
+  runInParallel(func1, func2)
+  #func3 starts & ends
+  p3.start()
+  p3.join()
